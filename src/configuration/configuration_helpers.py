8d082750ae837e67c7accb7367e6ce9154a7b928
zaksid <thezaksid@gmail.com>
2017-11-25 20:53:54 +0200

    Updated config file and config_helpers

--- src/configuration/configuration_helpers.py
+++ src/configuration/configuration_helpers.py
@@ -4,13 +4,71 @@ CONFIG_FILE = '../config.json'
 
 
 def load_config(config_file):
+    """
+    Load and parse JSON file with configuration.
+    :param config_file: JSON config file
+    :return: parsed JSON object
+    """
     return json.loads(open(config_file, 'r').read())
 
 
 def get_file_name():
+    """
+    Obtain relative path to video file.
+    :return: String with file path
+    """
     config = load_config(CONFIG_FILE)
     return config['video_path']
 
-def get_lanes():
+
+def get_camera_addr():
+    """
+    Obtain address of RTSP camera.
+    :return: String with camera address
+    """
+    config = load_config(CONFIG_FILE)
+    return config['camera_addr']
+
+
+def get_lanes_obj():
+    """
+    Get configuration of lanes.
+    :return: Object with lanes configuration
+    """
     config = load_config(CONFIG_FILE)
     return config['lanes']
+
+
+def get_save_format():
+    """
+    Get format for saving statistic info.
+    :return: String specifying save format
+    """
+    config = load_config(CONFIG_FILE)
+    return config['save_format']
+
+
+def get_small_vehicle_size():
+    """
+    Get size of small vehicle.
+    :return: Number specifying size of small vehicle
+    """
+    config = load_config(CONFIG_FILE)
+    sizes = config['vehicle_sizes']
+
+    for size in sizes:
+        if 'small' in size:
+            return size['small']
+
+
+def get_large_vehicle_size():
+    """
+    Get size of large vehicle.
+    :return: Number specifying size of large vehicle
+    """
+    config = load_config(CONFIG_FILE)
+    sizes = config['vehicle_sizes']
+
+    for size in sizes:
+        if 'large' in size:
+            return size['large']
